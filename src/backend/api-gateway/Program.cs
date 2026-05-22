4f525cd86c84f613901efa41c679e6e602c426e9
helinguler <gulerhelin@hotmail.com>
2026-05-22 22:30:49 +0300

    Configure API gateway routing

--- src/backend/api-gateway/Program.cs
+++ src/backend/api-gateway/Program.cs
@@ -1,6 +1,43 @@
+// frontendin doğrudan her mikroservise gitmesi yerine
+// requestleri önce bu gateway’e gönderiyor
+// gateway de istekleri ilgili backend servisine yönlendiriyor
+
 var builder = WebApplication.CreateBuilder(args);
+
+//  frontendin API Gatewaye istek atmasına izin verir
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+// gatewaye gelen requesti alır
+// appsettings.json içindeki ReverseProxy ayarlarına göre ilgili servise yönlendirir
+builder.Services
+    .AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors("AllowFrontend");
+
+app.MapGet("/", () => Results.Ok(new
+{
+    Service = "Invoice Management API Gateway",
+    Status = "Running",
+    Routes = new[]
+    {
+        "/api/auth",
+        "/api/customers",
+        "/api/invoices"
+    }
+}));
+
+app.MapReverseProxy();  // appsettings.json içindeki YARP route ayarlarını aktif eder
 
 app.Run();
